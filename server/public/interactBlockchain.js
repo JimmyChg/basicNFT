d1ca8941ce1a07ce9e13ea6281ce2a30ac831435
Jcheng <jimmy.cheng.75013@gmail.com>
2022-02-03 16:14:39 +0100

    chore: add ABI file of smart contract

--- server/public/interactBlockchain.js
+++ server/public/interactBlockchain.js
@@ -1,7 +1,10 @@
-import ABI from "./contractABI.json"
+// import requirejs from "requirejs"
+import * as ABI from './contractABI.json';
+// const ABI = requirejs("./contractABI.json")
 // const contract = new web3.eth.Contract([abi], address)
 
-
+const contractAddress = "0x71CF1e20763F5A933B24166443A067e2168cE893"
+console.log(ABI)
 if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
   }
